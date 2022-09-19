4986d3b7fabaa9fe937fec18424414588a4c7a50
zenekk11 <it.kenez@gmail.com>
2022-09-19 19:46:31 +0300

    hw1.1

---
+++ src/components/simpsons/simpson.js
@@ -0,0 +1,11 @@
+export default function simpson(props) {
+    let {name} = props;
+
+    let classNameItem = 'box'
+    return (
+        <div className={classNameItem}>
+        simpson
+        
+        </div>
+    );
+}
